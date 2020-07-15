06ff2bc44e1711a37b6615a0f7e39cf68220e287
Tim Betteridge <tbetter5@gmail.com>
2020-07-15 10:42:46 -0400

    Text file write
    Updating how the text file is written to

--- Func/MyFunc.cs
+++ Func/MyFunc.cs
@@ -9,12 +9,24 @@ namespace GroupTool.Func
     public class MyFunc
     {
 
-        public static string StringOut(string addstr,string id, string data)
+        public static string StringOut(string StrToAppend,string id, string data)
         {
+            string ou;
             //This formats a value for output
             //'[Name:] John Smith,'  
+            //StrToAppend - The text string to append to with new data
+            //id - The description of the dat
+            //data - the data. If the data is null then append null to StrToAppend and return
 
-            return addstr + "[" + id + ":] " + data + ", ";
+            if(data == string.Empty)
+            {
+                ou = StrToAppend;
+            }
+            else
+            {
+                ou = StrToAppend + "[" + id + ":] " + data + ", ";
+            }
+            return ou;
         }
         public static string[] SplitIt(string val, char sep)
         {
